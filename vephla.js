f42c5cdd0f2a962e109bdac980f2f92b621d7d1b
OluwatobiAkintolu <109463972+OluwatobiAkintolu@users.noreply.github.com>
2023-05-11 12:13:29 +0100

    Add files via upload

---
+++ vephla.js
@@ -0,0 +1,28 @@
+function myFunction1(a, b) {
+    return a + b;
+  }
+  document.getElementById("demo1").innerHTML = myFunction1(10, 3);
+
+
+
+  function myFunction2(a, b) {
+    return a - b;
+  }
+  document.getElementById("demo2").innerHTML = myFunction2(32, 14);
+
+
+  function myFunction3(a, b) {
+    return a * b;
+  }
+  document.getElementById("demo3").innerHTML = myFunction3(89, 14);
+
+  function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  document.getElementById("demo4").innerHTML = toCelsius(86);
+
+
+
+  
+
+  
